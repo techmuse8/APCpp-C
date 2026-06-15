1ae17c4984e8f00bf002b60f568e3c95d42ff114
techmuse <44119098+techmuse8@users.noreply.github.com>
2026-06-14 20:04:31 -0400

    Implement more functions and types
    This implements most of the functions and types up until the serverside section

--- APCpp_c.cpp
+++ APCpp_c.cpp
@@ -1,16 +1,42 @@
 #include "APCpp_c.h"
 #include <Archipelago.h>
+#include <map>
+#include <vector>
 
-static void (*c_item_clear_cb)() = nullptr;
-static void (*c_item_recv_cb)(int64_t,AP_C_Bool) = nullptr;
-static void (*c_loc_checked_cb)(int64_t) = nullptr;
+static void (*c_deathlink_recvex_cb)(const char* source, const char* cause) = nullptr;
+static void (*c_slot_data_int_cb)(int location) = nullptr;
+static void (*c_slot_data_map_intint_cb)(AP_C_MapIntInt*) = nullptr;
+static void (*c_slot_data_raw_cb)(const char*) = nullptr;
 
 static AP_C_Message sCurrentAPMessage;
+static std::vector<AP_C_IntIntPair> sCurrentSlotDataMap;
+
+static void convertMap(const std::map<int,int>& sourceMap, AP_C_MapIntInt& out) {
+    sCurrentSlotDataMap.clear();
+    sCurrentSlotDataMap.reserve(sourceMap.size());
+
+    for (const auto& [key, value] : sourceMap) {
+        sCurrentSlotDataMap.push_back({key, value});
+    }
+
+    out.items = sCurrentSlotDataMap.data();
+    out.size = sCurrentSlotDataMap.size();
+}
+
+static void SlotDataMapIntIntCallback(std::map<int,int> mapData) {
+    AP_C_MapIntInt slotData;
+    convertMap(mapData, slotData);
+    c_slot_data_map_intint_cb(&slotData);
+}
 
 extern "C" {
 
 void AP_C_Init(const char *ip, const char *game, const char *player_name, const char *passwd) {
     AP_Init(ip, game, player_name, passwd);
+}
+
+void AP_C_InitSinglePlayer(const char* filename) {
+    AP_Init(filename);
 }
 
 AP_C_Bool AP_C_IsInit() {
@@ -26,29 +52,49 @@ void AP_C_Shutdown() {
 }
 
 void AP_C_SetItemClearCallback(void (*cb)()) {
-    c_item_clear_cb = cb;
-
-    AP_SetItemClearCallback([](){
-        if (c_item_clear_cb)
-            c_item_clear_cb();
-    });
+    AP_SetItemClearCallback(cb);
 }
 
 void AP_C_SetItemRecvCallback(void (*cb)(int64_t,AP_C_Bool)) {
-    c_item_recv_cb = cb;
-
-    AP_SetItemRecvCallback([](int64_t item, bool notify){
-        if (c_item_recv_cb)
-            c_item_recv_cb(item, notify);
-    });
+    AP_SetItemRecvCallback(cb);
 }
 
 void AP_C_SetLocationCheckedCallback(void (*cb)(int64_t)) {
-    c_loc_checked_cb = cb;
+    AP_SetLocationCheckedCallback(cb);
+}
 
-    AP_SetLocationCheckedCallback([](int64_t loc){
-        if (c_loc_checked_cb)
-            c_loc_checked_cb(loc);
+void AP_C_SetDeathLinkRecvCallback(void (*f_deathrecv)()) {
+   AP_SetDeathLinkRecvCallback(f_deathrecv);
+}
+
+void AP_C_SetDeathLinkRecvCallbackEx(void (*f_deathrecv)(const char* source, const char* cause)) {
+    AP_SetDeathLinkRecvCallback([](std::string source, std::string cause){
+        if (c_deathlink_recvex_cb)
+            c_deathlink_recvex_cb(source.c_str(), cause.c_str());
+    });
+}
+
+void AP_C_RegisterSlotDataIntCallback(const char* key, void (*f_slotdata)(int location)) {
+    c_slot_data_int_cb = f_slotdata;
+
+    AP_RegisterSlotDataIntCallback(key, [](int location){
+        if (c_slot_data_int_cb)
+            c_slot_data_int_cb(location);
+    });
+}
+
+void AP_C_RegisterSlotDataMapIntIntCallback(const char* key, void(*f_slotdata)(AP_C_MapIntInt*)) {
+    c_slot_data_map_intint_cb = f_slotdata;
+
+    AP_RegisterSlotDataMapIntIntCallback(key, SlotDataMapIntIntCallback);
+}
+
+void AP_C_RegisterSlotDataRawCallback(const char* key, void (*f_slotdata)(const char*)) {
+    c_slot_data_raw_cb = f_slotdata;
+
+    AP_RegisterSlotDataRawCallback(key, [](std::string data){
+        if (c_slot_data_raw_cb)
+            c_slot_data_raw_cb(data.c_str());
     });
 }
 
@@ -67,6 +113,19 @@ void AP_C_SendItem(int64_t location) {
 void AP_C_StoryComplete() {
     AP_StoryComplete();
 }
+
+AP_C_Bool AP_C_DeathLinkPending() {
+    return AP_DeathLinkPending();
+}
+
+void AP_C_DeathLinkClear() {
+    AP_DeathLinkClear();
+}
+
+void AP_C_DeathLinkSend(const char* cause) {
+    AP_DeathLinkSend(cause);
+}
+
 
 AP_C_Bool AP_C_IsMessagePending() {
     return AP_IsMessagePending();
@@ -89,5 +148,16 @@ void AP_C_Say(const char* text) {
     AP_Say(text);
 }
 
+AP_C_ConnectionStatus AP_C_GetConnectionStatus() {
+    return static_cast<AP_C_ConnectionStatus>(AP_GetConnectionStatus());
+}
+
+uint64_t AP_C_GetUUID() {
+    return AP_GetUUID();
+}
+
+int AP_C_GetPlayerID() {
+    return AP_GetPlayerID();
+}
 
 }
--- APCpp_c.h
+++ APCpp_c.h
@@ -7,13 +7,13 @@ extern "C" {
 
 typedef uint8_t AP_C_Bool;
 
-struct AP_C_NetworkVersion {
+typedef struct AP_C_NetworkVersion {
     int major;
     int minor;
     int build;
-};
+} AP_C_NetworkVersion;
 
-struct AP_C_NetworkItem {
+typedef struct AP_C_NetworkItem {
     int64_t item;
     int64_t location;
     int player;
@@ -21,39 +21,105 @@ struct AP_C_NetworkItem {
     char* itemName;
     char* locationName;
     char* playerName;
-};
+} AP_C_NetworkItem;
 
-enum AP_C_MessageType {
-    Plaintext, ItemSend, ItemRecv, Hint, Countdown
-};
+typedef struct AP_C_IntIntPair {
+    int key;
+    int value;
+} AP_C_IntIntPair;
+
+typedef struct AP_C_MapIntInt {
+    AP_C_IntIntPair* items;
+    int size;
+} AP_C_MapIntInt;
 
 void AP_C_Init(const char* ip, const char* game, const char* player_name, const char* passwd);
+void AP_C_InitSinglePlayer(const char* filename);
 AP_C_Bool AP_C_IsInit();
 void AP_C_Start();
 void AP_C_Shutdown();
+
+/* Configuration Functions */
+
+void AP_C_EnableQueueItemRecvMsgs(AP_C_Bool enable);
+void AP_C_SetDeathLinkSupported(AP_C_Bool enable);
+
+/* Required Callback Functions */
 
 void AP_C_SetItemClearCallback(void (*cb)());
 void AP_C_SetItemRecvCallback(void (*cb)(int64_t item, AP_C_Bool notify));
 void AP_C_SetLocationCheckedCallback(void (*cb)(int64_t location));
 
-void AP_C_EnableQueueItemRecvMsgs(AP_C_Bool enable);
-void AP_C_SetDeathLinkSupported(AP_C_Bool enable);
+/* Optional Callback Functions */
+
+void AP_C_SetDeathLinkRecvCallback(void (*f_deathrecv)());
+void AP_C_SetDeathLinkRecvCallbackEx(void (*f_deathrecv)(const char*, const char*));
+
+// Parameter Function receives Slotdata of respective type
+void AP_C_RegisterSlotDataIntCallback(const char* key, void (*f_slotdata)(int location));
+void AP_C_RegisterSlotDataMapIntIntCallback(const char* key, void(*f_slotdata)(AP_C_MapIntInt*));
+void AP_C_RegisterSlotDataRawCallback(const char* key, void (*f_slotdata)(const char*));
+
+/* Game Management Functions */
 
 void AP_C_SendItem(int64_t location);
+//void AP_C_SendItemSets(int64_t location); // TODO: Implement this
 void AP_C_StoryComplete();
 
-AP_C_Bool AP_C_IsMessagePending();
-void AP_C_ClearLatestMessage();
+/* Deathlink Functions */
+
+AP_C_Bool AP_C_DeathLinkPending();
+void AP_C_DeathLinkClear();
+void AP_C_DeathLinkSend(const char* cause);
+
+/* Message Management Types */
+typedef enum AP_C_MessageType {
+    AP_MsgPlaintext, AP_MsgItemSend, AP_MsgItemRecv, AP_MsgHint, AP_MsgCountdown
+} AP_C_MessageType;
 
 struct AP_C_Message {
     AP_C_MessageType type;
     const char* text;
 };
 
+/* Message Management Functions */
+
+AP_C_Bool AP_C_IsMessagePending();
+void AP_C_ClearLatestMessage();
 AP_C_Message* AP_C_GetLatestMessage();
 
 void AP_C_Say(const char* text);
 
+/* Connection Information Types */
+
+typedef enum AP_C_ConnectionStatus {
+    AP_Disconnected, AP_Connected, AP_Authenticated, AP_ConnectionRefused
+} AP_C_ConnectionStatus;
+
+// #define AP_PERMISSION_DISABLED 0b000
+// #define AP_PERMISSION_ENABLED 0b001
+// #define AP_PERMISSION_GOAL 0b010
+// #define AP_PERMISSION_AUTO 0b110
+
+// struct AP_RoomInfo {
+//     AP_NetworkVersion version;
+//     std::vector<std::string> tags;
+//     bool password_required;
+//     std::map<std::string, int> permissions;
+//     int hint_cost;
+//     int location_check_points;
+//     //MISSING: games
+//     std::map<std::string, std::string> datapackage_checksums;
+//     std::string seed_name;
+//     double time;
+// };
+
+/* Connection Information Functions */
+
+//int AP_GetRoomInfo(AP_RoomInfo*); //TODO: Implement
+AP_C_ConnectionStatus AP_C_GetConnectionStatus();
+uint64_t AP_C_GetUUID();
+int AP_C_GetPlayerID();
 
 #ifdef __cplusplus
 }
