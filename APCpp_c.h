647ca30c4ab890acd0c88f9aa8df5cd9133e472a
techmuse <44119098+techmuse8@users.noreply.github.com>
2026-06-18 10:52:03 -0400

    Implement "APC_DYNAMIC_MODE" option
    "APC_DYNAMIC_MODE" lets you choose between either resolving all the APCpp-C function exports manually at runtime (LoadLibrary, dlsym, etc.) or link time

--- APCpp_c.h
+++ APCpp_c.h
@@ -33,46 +33,54 @@ typedef struct AP_C_MapIntInt {
     int size;
 } AP_C_MapIntInt;
 
-void AP_C_SetClientVersion(AP_C_NetworkVersion* version);
+// "APC_DYNAMIC_MODE" lets you choose between either resolving all the APCpp-C
+// public functions manually at runtime (LoadLibrary, dlsym, etc.) or link time 
+#ifdef APC_DYNAMIC_MODE
+#define DECL_APC_FUNCTION(retType, func, ...)     extern retType (* func)(__VA_ARGS__)
+#else
+#define DECL_APC_FUNCTION(retType, func, ...)     retType func(__VA_ARGS__)
+#endif
 
-void AP_C_Init(const char* ip, const char* game, const char* player_name, const char* passwd);
-void AP_C_InitSinglePlayer(const char* filename);
-AP_C_Bool AP_C_IsInit();
-void AP_C_Start();
-void AP_C_Shutdown();
+DECL_APC_FUNCTION(void, AP_C_SetClientVersion, AP_C_NetworkVersion* version);
+
+DECL_APC_FUNCTION(void, AP_C_Init, const char* ip, const char* game, const char* player_name, const char* passwd);
+DECL_APC_FUNCTION(void, AP_C_InitSinglePlayer, const char* filename);
+DECL_APC_FUNCTION(AP_C_Bool, AP_C_IsInit);
+DECL_APC_FUNCTION(void, AP_C_Start);
+DECL_APC_FUNCTION(void, AP_C_Shutdown);
 
 /* Configuration Functions */
 
-void AP_C_EnableQueueItemRecvMsgs(AP_C_Bool enable);
-void AP_C_SetDeathLinkSupported(AP_C_Bool enable);
+DECL_APC_FUNCTION(void, AP_C_EnableQueueItemRecvMsgs, AP_C_Bool enable);
+DECL_APC_FUNCTION(void, AP_C_SetDeathLinkSupported, AP_C_Bool enable);
 
 /* Required Callback Functions */
 
-void AP_C_SetItemClearCallback(void (*cb)());
-void AP_C_SetItemRecvCallback(void (*cb)(int64_t item, AP_C_Bool notify));
-void AP_C_SetLocationCheckedCallback(void (*cb)(int64_t location));
+DECL_APC_FUNCTION(void, AP_C_SetItemClearCallback, void (*cb)());
+DECL_APC_FUNCTION(void, AP_C_SetItemRecvCallback, void (*cb)(int64_t item, AP_C_Bool notify));
+DECL_APC_FUNCTION(void, AP_C_SetLocationCheckedCallback, void (*cb)(int64_t location));
 
 /* Optional Callback Functions */
 
-void AP_C_SetDeathLinkRecvCallback(void (*f_deathrecv)());
-void AP_C_SetDeathLinkRecvCallbackEx(void (*f_deathrecv)(const char*, const char*));
+DECL_APC_FUNCTION(void, AP_C_SetDeathLinkRecvCallback, void (*f_deathrecv)());
+DECL_APC_FUNCTION(void, AP_C_SetDeathLinkRecvCallbackEx, void (*f_deathrecv)(const char*, const char*));
 
 // Parameter Function receives Slotdata of respective type
-void AP_C_RegisterSlotDataIntCallback(const char* key, void (*f_slotdata)(int location));
-void AP_C_RegisterSlotDataMapIntIntCallback(const char* key, void(*f_slotdata)(AP_C_MapIntInt*));
-void AP_C_RegisterSlotDataRawCallback(const char* key, void (*f_slotdata)(const char*));
+DECL_APC_FUNCTION(void, AP_C_RegisterSlotDataIntCallback, const char* key, void (*f_slotdata)(int location));
+DECL_APC_FUNCTION(void, AP_C_RegisterSlotDataMapIntIntCallback, const char* key, void(*f_slotdata)(AP_C_MapIntInt*));
+DECL_APC_FUNCTION(void, AP_C_RegisterSlotDataRawCallback, const char* key, void (*f_slotdata)(const char*));
 
 /* Game Management Functions */
 
-void AP_C_SendItem(int64_t location);
-//void AP_C_SendItemSets(int64_t location); // TODO: Implement this
-void AP_C_StoryComplete();
+DECL_APC_FUNCTION(void, AP_C_SendItem, int64_t location);
+//DECL_APC_FUNCTION(void, AP_C_SendItemSets, int64_t location); // TODO: Implement this
+DECL_APC_FUNCTION(void, AP_C_StoryComplete);
 
 /* Deathlink Functions */
 
-AP_C_Bool AP_C_DeathLinkPending();
-void AP_C_DeathLinkClear();
-void AP_C_DeathLinkSend(const char* cause);
+DECL_APC_FUNCTION(AP_C_Bool, AP_C_DeathLinkPending);
+DECL_APC_FUNCTION(void, AP_C_DeathLinkClear);
+DECL_APC_FUNCTION(void, AP_C_DeathLinkSendconst, char* cause);
 
 /* Message Management Types */
 typedef enum AP_C_MessageType {
@@ -86,11 +94,11 @@ struct AP_C_Message {
 
 /* Message Management Functions */
 
-AP_C_Bool AP_C_IsMessagePending();
-void AP_C_ClearLatestMessage();
-AP_C_Message* AP_C_GetLatestMessage();
+DECL_APC_FUNCTION(AP_C_Bool, AP_C_IsMessagePending);
+DECL_APC_FUNCTION(void, AP_C_ClearLatestMessage);
+DECL_APC_FUNCTION(AP_C_Message*, AP_C_GetLatestMessage);
 
-void AP_C_Say(const char* text);
+DECL_APC_FUNCTION(void, AP_C_Say, const char* text);
 
 /* Connection Information Types */
 
@@ -119,9 +127,9 @@ typedef enum AP_C_ConnectionStatus {
 /* Connection Information Functions */
 
 //int AP_GetRoomInfo(AP_RoomInfo*); //TODO: Implement
-AP_C_ConnectionStatus AP_C_GetConnectionStatus();
-uint64_t AP_C_GetUUID();
-int AP_C_GetPlayerID();
+DECL_APC_FUNCTION(AP_C_ConnectionStatus, AP_C_GetConnectionStatus);
+DECL_APC_FUNCTION(uint64_t, AP_C_GetUUID);
+DECL_APC_FUNCTION(int, AP_C_GetPlayerID);
 
 #ifdef __cplusplus
 }
